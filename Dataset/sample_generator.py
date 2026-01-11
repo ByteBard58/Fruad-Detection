f30e5fb6793877ab903a8ff07b6d4ffc01ca03fd
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-01-11 17:12:35 +0600

    Update sample_generator to add noise in the generated sample datasets

--- Dataset/sample_generator.py
+++ Dataset/sample_generator.py
@@ -7,9 +7,11 @@ These samples are used for testing the app.
 
 import os
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 
 load_dotenv()
+rng = np.random.default_rng(seed=12349)
 
 data_path: str = os.getenv("DATA_PATH")
 df_main: pd.DataFrame = pd.read_csv(data_path)
@@ -18,6 +20,13 @@ print(f"Original shape: {df_main.shape}")
 df_sampled: pd.DataFrame = df_main.sample(n=80)
 
 df_return: pd.DataFrame = df_sampled.drop(columns=["id","Class"])
+
+def add_minus(x):
+  val = rng.uniform(low=-2.0,high=2.0)
+  return x+val
+
+df_return = df_return.map(add_minus)
+
 print(f"Sampled shape: {df_return.shape}")
 
 df_return.to_csv("Dataset/sample_data.csv",index=False)
